2df29e41c8c4e216f5ff32b80afc2c4bb457c72e
Noah <noahmwangi@gmail.com>
2023-03-10 19:33:32 +0300

    Works fine so far, need to add the message holder array messages[]

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+function writeCards(cards, celebration){
+    for (let i = 0; i <cards.length; i++){
+        console.log(`Thank you, ${cards[i]} for the wonderful ${celebration} gift!`);
+        debugger;
+    } 
+    return cards;
+}
+writeCards(cards, "celebration");
+
